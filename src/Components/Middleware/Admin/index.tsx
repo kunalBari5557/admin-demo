987750378b08627b9f1da4b2299cf3218afa7b8b
kunal <kunalaage21@gmail.com>
2023-08-14 17:50:51 +0530

    hotfix: login auth path

--- src/Components/Middleware/Admin/index.tsx
+++ src/Components/Middleware/Admin/index.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router";
 function AdminMiddleware() {
     let auth_token=localStorage.getItem('Token')
   return (
-    auth_token?<Outlet/>:<Navigate to={"/admin/login"}/>
+    auth_token?<Outlet/>:<Navigate to={"/"}/>
   ) 
 }
 
